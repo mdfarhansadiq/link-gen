be9e4079c71185c6a66abcdd3bd5b4f5f4da26eb
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2024-01-25 00:32:49 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,20 @@ var anchorTag = '';
 function linkGenerator() {
   var websiteLink = document.getElementById("webLink").value;
   var websiteName = document.getElementById("websiteName").value;
-  anchorTag = "<a href='" + websiteLink + "' target='_blank'>" + websiteName + "</a>";
-
-  if(websiteLink != '' && websiteName != '')
-  {
-    document.getElementById("copyBtn").style.display = 'block';
+  var checkHttps = websiteLink.slice(0, 8);
+  if (checkHttps === "https://") {
+    anchorTag = '<a href="' + websiteLink + '" target="_blank">' + websiteName + '</a>';
   }
+  else {
+    anchorTag = '<a href="https://' + websiteLink + '" target="_blank">' + websiteName + '</a>';
+  }
+  document.getElementById("copyBtn").style.display = 'block';
+  document.getElementById("webAnchor").innerHTML = anchorTag;
+  
 }
 
 
-// Add event listener to the form to prevent submission on page load   
+// Add event listener to the form to prevent submission on page load
 document.getElementById("linkGeneratorForm").addEventListener("submit", function(event) {
   event.preventDefault();
 });
@@ -23,5 +27,4 @@ document.getElementById("linkGeneratorForm").addEventListener("submit", function
 function valGet() {
   navigator.clipboard.writeText(anchorTag)
   document.getElementById("message").style.display = "block";
-  document.getElementById("message").innerHTML = 'Copied';
 }
